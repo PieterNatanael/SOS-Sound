6912b843685ff6ce01b4d998853eec710da30095
PieterNatanael <pieternatanael@icloud.com>
2024-07-25 15:48:02 +0800

    Initial Commit

---
+++ SOS Sound/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SOS Sound
+//
+//  Created by Pieter Yoshua Natanael on 25/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SOS Sound/SOS_SoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOS_SoundApp.swift
+//  SOS Sound
+//
+//  Created by Pieter Yoshua Natanael on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SOS_SoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
